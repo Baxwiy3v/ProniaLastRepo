c9b94c01b075dfc2233a09160e97a3454c2d8678
Baxwiy3v_ <tu7nxadgf@code.edu.az>
2023-12-05 22:26:41 +0400

    Login Completed

--- AB460Proniya/Controllers/AccountController.cs
+++ AB460Proniya/Controllers/AccountController.cs
@@ -8,14 +8,14 @@ namespace AB460Proniya.Controllers
 {
     public class AccountController : Controller
     {
-		private readonly UserManager<AppUser> _manager;
-		private readonly SignInManager<AppUser> _signIn;
+        private readonly UserManager<AppUser> _manager;
+        private readonly SignInManager<AppUser> _signIn;
 
-		public AccountController(UserManager<AppUser> manager,SignInManager<AppUser> signIn)
+        public AccountController(UserManager<AppUser> manager, SignInManager<AppUser> signIn)
         {
-			_manager = manager;
-			_signIn = signIn;
-		}
+            _manager = manager;
+            _signIn = signIn;
+        }
         public IActionResult Register()
         {
             return View();
@@ -27,7 +27,7 @@ namespace AB460Proniya.Controllers
 
             if (!ModelState.IsValid) return View();
 
-            uservm.Surname=uservm.Surname.Trim();
+            uservm.Surname = uservm.Surname.Trim();
 
             uservm.Name = uservm.Name.Trim();
 
@@ -40,23 +40,23 @@ namespace AB460Proniya.Controllers
                 .ToUpper(uservm.Name[0]) + uservm.Name
                 .Substring(1)
                 .ToLower();
-          
+
 
 
             AppUser user = new AppUser
-            { 
-              Name = name,
-              Surname = surname,
-              UserName = uservm.UserName,
-              Email = uservm.Email,
-              Gender=uservm.Gender
+            {
+                Name = name,
+                Surname = surname,
+                UserName = uservm.UserName,
+                Email = uservm.Email,
+                Gender = uservm.Gender
 
-            
+
             };
 
-         IdentityResult result=  await _manager.CreateAsync(user,uservm.Password);
+            IdentityResult result = await _manager.CreateAsync(user, uservm.Password);
 
-            if(!result.Succeeded)
+            if (!result.Succeeded)
             {
 
                 foreach (IdentityError error in result.Errors)
@@ -69,17 +69,77 @@ namespace AB460Proniya.Controllers
             }
             await _signIn.SignInAsync(user, isPersistent: false);
 
-            return RedirectToAction("Index","Home");
+            return RedirectToAction("Index", "Home");
 
 
         }
-		public async Task<IActionResult> Logout()
-		{
-			await _signIn.SignOutAsync();
-
-			return RedirectToAction("Index", "Home");
-		}
 
 
-	}
+
+        public IActionResult Login()
+        {
+
+
+            return View();
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginvm, string? returnUrl)
+        {
+
+            if (!ModelState.IsValid) return View();
+
+            AppUser user = await _manager.FindByNameAsync(loginvm.UserOrEmail);
+
+            if(user == null)
+            {
+
+                user = await _manager.FindByEmailAsync(loginvm.UserOrEmail);
+                if (user == null)
+                {
+                    ModelState.AddModelError(String.Empty, "UserName, Email or Password is wrong");
+
+                    return View();
+
+                }
+            }
+
+          var result = await _signIn.PasswordSignInAsync(user, loginvm.Password, loginvm.IsRemember, true);
+
+            if (result.IsLockedOut)
+            {
+                ModelState.AddModelError(String.Empty, "Həddindən artıq cəhd,daha sonra yenidən cəhd edin");
+                return View();
+            }
+
+            if (!result.Succeeded)
+            {
+
+                ModelState.AddModelError(String.Empty, "UserName, Email or Password is wrong");
+                return View();
+
+            }
+
+
+            if (returnUrl is null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return Redirect(returnUrl);
+
+        }
+
+
+
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signIn.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+    }
 }
---
+++ AB460Proniya/ModelsVM/Account/LoginVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AB460Proniya.ModelsVM
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage= "User ve ye Email daxil edilmelidir")]
+
+        public string UserOrEmail { get; set; }
+        [Required(ErrorMessage = "Password daxil edilmelidir")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool IsRemember { get; set; }
+    }
+}
